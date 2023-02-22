4aaa829b773b797aa829eb39fbafc1444f3d83ec
tusharravenclaw <tushar68053@gmail.com>
2023-02-22 07:56:25 +0530

    functional except once edge case, clear & decimal

--- script.js
+++ script.js
@@ -1,104 +1,104 @@
+// For DOM manipulation
 const numberBtn = document.querySelectorAll("button")
 const displayCurrent = document.querySelector(".display-current")
 const displayLast = document.querySelector(".display-last")
 
-// to access them globally
+// Dummy content to not let the empty display minimize
+displayCurrent.textContent = '\u00a0';
+displayLast.textContent = '\u00a0';
+
+// Access variables globally
 let newNumber = '';
 let currentNumber = '';
 let operator = '';
 let results;
 let a;
 let b = '';
+let bCase = false;
 
+// Select all button without individual ID or class
 for(let i = 0; i < numberBtn.length; i++){
     numberBtn[i].addEventListener('click', function(e){
         display(e)
     });
 }
 
-// to not let the display minimize
-displayCurrent.textContent = '\u00a0';
-displayLast.textContent = '\u00a0';
-
+// to show results
 function display(e){
 
-    let button = e.target.id;
+    let button = e.target.id; // Access any button through it's ID
 
-    if(button == "delete"){
+    // if-else cases for operators, numbers, delete, and equal button
+
+    if(button == "delete"){ // to start from scratch
         location.reload()
 
     } else if (button == "+" || button == "-" || button == "÷" || button == "×"){
         
+        // if-else case for first/fresh operation and further operations
+
         if (operator == ""){
-            operator = e.target.id;
-            console.log(operator);
-            displayCurrent.textContent += newNumber + operator;
-            currentNumber += newNumber + operator;
+            firstOperation(e)
 
         } else {
-
-            if (!b){
-                operator = e.target.id
-                x = results
-                console.log(x);
-                results = operate(operator,x,currentNumber)
-                console.log(results)
-                displayCurrent.textContent = '\u00a0';
-                displayLast.textContent = results + operator;
-                currentNumber = ''
-                a = results;
-
-                if(operator == "+" || operator == "-"){
-                    b = 0;
-                }
-                else if (operator == "÷" || operator == "×"){
-                    b = 1;
-                }
-
-            } else {
+                console.log(operator)
                 results = operate(operator,a,b)
                 operator = e.target.id
-                displayCurrent.textContent = '\u00a0';
-                displayLast.textContent = results + operator;
-                currentNumber = ''
-                a = results;
-
-                if(operator == "+" || operator == "-"){
-                    b = 0;
-                }
-                else if (operator == "÷" || operator == "×"){
-                    b = 1;
-                }
-            }
+                furtherOperations()
         }
-        console.log(b)
 
     } else if(button == "="){
         
+        // If-else cases for pressing equal button without any second number
         if (!b){
             displayCurrent.textContent = a;
             displayLast.textContent = '\u00a0';
+
         } else {
-        results = operate(operator,a,b)
-        displayLast.textContent = a + operator + b + button;
-        displayCurrent.textContent = results;
+            results = operate(operator,a,b)
+            displayLast.textContent = a + operator + b + button;
+            displayCurrent.textContent = results;
         }
-        currentNumber = '';
+
+        currentNumber = ''
 
     } else { // case for all the numbers
 
         newNumber = e.target.id;
         currentNumber += newNumber;
         displayCurrent.textContent = currentNumber;
+
+        // a is only required for first operation
         if (!operator){
             a = currentNumber;
         } else {
             b = currentNumber.slice(a.length + 1) // +1 for removing operator for the string
-            console.log(b)
         }
         newNumber = ''
-        console.log(currentNumber)
-        console.log(operator)
+    }
+}
+
+// To run the first or fresh operation
+
+function firstOperation(e){
+    operator = e.target.id;
+    displayCurrent.textContent += newNumber + operator;
+    currentNumber += newNumber + operator;
+    console.log(operator)
+}
+
+// To run operations after the first operation
+function furtherOperations() {
+    displayCurrent.textContent = '\u00a0';
+    displayLast.textContent = results + operator;
+    currentNumber = ''
+    a = results;
+
+    if(operator == "+" || operator == "-"){
+        b = 0;
+    }
+    else if (operator == "÷" || operator == "×"){
+        b = 1;
     }
 }
 
@@ -119,7 +119,7 @@ function divide(a,b){
     return (a/b);
 }
 
-// Single Operator Function
+// Single Operate Function
 function operate(operator,a,b){
 
     if(operator == "+"){
